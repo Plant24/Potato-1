d3bb0422241e31b63c5af3b424b168adbb22077c
Plant24 <75841437+Plant24@users.noreply.github.com>
2020-12-28 00:11:25 +0200

    first commit

---
+++ src/model/classes.js
@@ -0,0 +1,9 @@
+/* eslint-disable */
+// Theses classes should correspond to the softmax output of your model.
+
+export const MODEL_CLASSES = {
+  0: 'Early_blight',
+  1: 'Healthy',
+  2: 'Late Blight',
+
+};
